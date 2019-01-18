1be0bd8b215ec4843d6f975b2474210e50a6b704
Brendon Welker <wel15018@byui.edu>
2019-01-18 16:10:52 -0700

    switching order of nav bar links

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
 <?php
 echo "<ul>
-<li><a class='home' href='homepage.php'>Home</a></li>
-<li><a href=''>About Me</a></li>
 <li><a href=''>Assignments</a></li>
+<li><a href=''>About Me</a></li>
+<li><a class='home' href='homepage.php'>Home</a></li>
 </ul>";
 ?>
