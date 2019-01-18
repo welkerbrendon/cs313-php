c38837a8fc21be948f4084c7bbb5b888720cb777
Brendon Welker <wel15018@byui.edu>
2019-01-18 15:08:32 -0700

    closing tag

--- web/nav.php
+++ web/nav.php
@@ -1,3 +1,7 @@
 <?php
-echo "<ul><li><a href="">Home</a></li><li><a href="">About Me</a></li><li><a href="">Assignments</a></li>";
+echo "<ul>
+<li><a href="">Home</a></li>
+<li><a href="">About Me</a></li>
+<li><a href="">Assignments</a></li>
+</ul>";
 ?>
