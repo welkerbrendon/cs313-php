5893e9e6bb45eab280e6aa2a42ade8e60cf9885a
Brendon Welker <wel15018@byui.edu>
2019-01-18 15:13:52 -0700

    adding link to homepage

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
 <?php
-echo "<ul>
+echo ("<ul>
 <li><a href="homepage.php">Home</a></li>
 <li><a href="homepage.php">About Me</a></li>
 <li><a href="homepage.php">Assignments</a></li>
-</ul>";
+</ul>");
 ?>
