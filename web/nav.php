b6774de154388d300c32922394a4949e6ebbc5b4
Brendon Welker <wel15018@byui.edu>
2019-01-18 15:59:03 -0700

    making home different

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
 <?php
 echo "<ul>
 <li><a href='homepage.php'>Home</a></li>
-<li><a href=''>About Me</a></li>
-<li><a href=''>Assignments</a></li>
+<li><a class='nav' href=''>About Me</a></li>
+<li><a class='nav' href=''>Assignments</a></li>
 </ul>";
 ?>
