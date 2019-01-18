3bf6d3dced6d0bb08dcd647989a08ed93badf7af
Brendon Welker <wel15018@byui.edu>
2019-01-18 16:54:53 -0700

    position

--- web/nav.php
+++ web/nav.php
@@ -3,7 +3,7 @@ echo "<div>
 <a class='logo' href='homepage.php'><img src=BMW-Emblem.jpg></a>
 </div>
 <ul>
-<li><a href=''>Assignments</a></li>
+<li><a href='homepage.php'>Assignments</a></li>
 <li><a href=''>About Me</a></li>
 <li><a class='home' href='homepage.php'>Home</a></li>
 </ul>";
