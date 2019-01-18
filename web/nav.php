fe407d95a9c083a20cb2c6abfb6ea7878c7d7c51
Brendon Welker <wel15018@byui.edu>
2019-01-18 15:25:52 -0700

    making nav bar links

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
 <?php
 echo "<ul>
 <li><a href='homepage.php'>Home</a></li>
-<li>About Me</li>
-<li>Assignments</li>
+<li><a href=''>About Me</a></li>
+<li><a href=''>Assignments</a></li>
 </ul>";
 ?>
