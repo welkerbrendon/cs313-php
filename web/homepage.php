7fab2beb5d7997ef1630c574f2a701ec4c0460ac
Brendon Welker <wel15018@byui.edu>
2019-01-18 23:52:15 -0700

    adding introduction content

--- web/homepage.php
+++ web/homepage.php
@@ -8,5 +8,11 @@
         <?php include 'nav.php';?>
         <img class="banner" src="weddingpic.jpg">
         <h1>Get ready to get to know the great Brendon Welker!</h1>
+        <p>This website is dedicated to the brilliant Brendon Welker.
+            It will give information as to who he is and it will
+            showcase his web engineering skills. Feel free to get to
+            know the man, the myth, the legend. Then see his skills in
+            action through his class assignments which can be accessed here.
+        </p>
     </body>
 </html>
