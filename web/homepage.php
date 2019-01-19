141cf26cde5db8413e610372b2cad2cf31e2de0c
Brendon Welker <wel15018@byui.edu>
2019-01-19 12:31:04 -0700

    adjusting table properties

--- web/homepage.php
+++ web/homepage.php
@@ -8,7 +8,7 @@
         <?php include 'nav.php';?>
         <img class="banner" src="weddingpic.jpg">
         <h1>Get ready to get to know the great Brendon Welker!</h1>
-        <p>This website is dedicated to the brilliant Brendon Welker.
+        <p class="intro">This website is dedicated to the brilliant Brendon Welker.
             It will give information as to who he is and it will
             showcase his web engineering skills. Feel free to get to
             know the man, the myth, the legend. Then see his skills in
