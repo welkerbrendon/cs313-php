7b9c67c450404bf0c58aa62de9c67274c30079ad
Brendon Welker <wel15018@byui.edu>
2019-01-18 17:43:32 -0700

    trying to add dropdown

--- web/nav.php
+++ web/nav.php
@@ -2,16 +2,16 @@
 echo "
 <a class='logo' href='homepage.php'><img src=BMW-Emblem.jpg></a>
 <ul>
-<li><a href='' class='dropdown'>Assignments</button></li>
+<li><div><a href='' class='dropdown'>Assignments</button></li>
 <div class='dropdown-links'>
 <a href=''>assignment1</a>
-</div>
-<li><a href='' class='dropdown'>About Me</button></li>
+</div></div>
+<li><div><a href='' class='dropdown'>About Me</button></li>
 <div class='dropdown-links'>
 <a href=''>Wife</a>
 <a href=''>Sports</a>
 <a href=''>Cars</a>
-</div>
+</div></div>
 <li><a class='home' href='homepage.php'>Home</a></li>
 </ul>";
 ?>
