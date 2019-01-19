7f1c23256be4a085d46d051d1d29f70042a3184a
Brendon Welker <wel15018@byui.edu>
2019-01-18 22:56:06 -0700

    code developed from w3 schools

--- web/nav.php
+++ web/nav.php
@@ -1,19 +1,25 @@
 <?php
 echo "
 <a class='logo' href='homepage.php'><img src=BMW-Emblem.jpg></a>
-<ul>
-<li><div><a href='' class='dropdown'>Assignments</button></li>
-<ul class='choices'>
-<li><a href=''>assignment1</a></li>
-</ul>
-</div>
-<li><div><a href='' class='dropdown'>About Me</button></li>
-<ul class='choices'>
-<li><a href=''>Wife</a></li>
-<li><a href=''>Sports</a></li>
-<li><a href=''>Cars</a></li>
-</ul>
-</div>
-<li><a class='home' href='homepage.php'>Home</a></li>
-</ul>";
+<div class='navbar'>
+  <div class='dropdown' id='first-dropdwn'>
+    <button class='dropbtn'>Assignments 
+      <i class='fa fa-caret-down'></i>
+    </button>
+    <div class='dropdown-content' id='first-content'>
+      <a href='#'>Assignment1</a>
+    </div>
+  </div> 
+  <div class='dropdown' id='second-dropdwn'>
+    <button class='dropbtn'>About Me 
+      <i class='fa fa-caret-down'></i>
+    </button>
+    <div class='dropdown-content' id='second-content'>
+      <a href='#'>Testimony</a>
+      <a href='#'>Wife</a>
+      <a href='#'>Hobbies</a>
+    </div>
+  </div> 
+  <a href='#home'>Home</a>
+</div>";
 ?>
