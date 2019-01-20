d7e450302782bc82e1d3650aecb0876672498be1
Brendon Welker <wel15018@byui.edu>
2019-01-19 19:48:06 -0700

    hobbies banner

--- web/aboutme/wife.php
+++ web/aboutme/wife.php
@@ -3,6 +3,7 @@
     <head>
         <title>Brendon Welker: Better Half</title>
         <link rel="stylesheet" href="../home/navbar.css">
+        <link rel="stylesheet" href="wife.css"
     </head>
     <body>
         <?php include '../home/nav.php';?>
