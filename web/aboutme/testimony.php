e54f29c13f6be8f72d17be6e552f262ef1f60002
Brendon Welker <wel15018@byui.edu>
2019-01-19 18:05:32 -0700

    adding content to testimony

--- web/aboutme/testimony.php
+++ web/aboutme/testimony.php
@@ -7,5 +7,49 @@
     <body>
         <?php include '../home/nav.php';?>
         <img class="banner" src="../pictures/ventura.jpg">
+        <h1>The Church of Jesus Christ of Latter-day Saints</h1>
+        <p>I believe in Jesus Christ. I believe He is the savior of the world.
+        I believe He gives me the opportunity to repent and change each
+        and every day so I can become a better man. I also believe He has a
+        specific set of teaching He wants taught to all and I believe He has
+        given us prophets again in order to keep these teachings pure. I believe
+        this prophet and these teachings can be found in The Church of Jesus Christ
+        of Latter-day Saints. I believe this has affected every part of my life and
+        I believe everything good in my life has been a product of this belief.</p>
+        <h2>Some of the reasons I have this belief:</h2>
+        <h3>Childhood</h3>
+        <p>I was born into a family who attended The Church of Jesus Christ of
+        Latter-day Saints. Because of this I was taken to this church with my family
+        durring my childhood. Even though I stopped going when I started reaching my
+        early teenage years, I never forgot the things which I learned going to the
+        services and more importantly, I never forgot the feelings I had when I lived
+        those teachings.</p>
+        <h3>The Book of Mormon</h3>
+        <section>
+            <div class="left">
+                <img src="../pictures/BOM.jpg">
+            </div>
+            <div class="right">
+            <p>Eventually, in my later teenage years, I started attending The Church of
+            Jesus Christ of Latter-day Saints again. This lead to some of those feelings
+            I had when I was younger returning. I noticed there was a joy which I had
+            been missing when I was not living the things taught through the modern
+            prophets. Because of this I decided to find for myself if it was simply something
+            good or if it was actually all what it claimed to be. I decided to know once and
+            for all if The Church of Jesus Christ of Latter-day Saints was really the only
+            true and living church on the face of the Earth and the only one authorized by
+            God to act in the His name. So I read The Book of Mormon: Another Testament of
+            Jesus Christ and prayed to know if it was true and if what it contained was fact
+            or fiction. I felt without a doubt it was true.</p>
+            </div>
+        </section>
+        <h3>The Great California Ventura Mission</h3>
+        <p>After I felt it was all true I felt the need to share that with as many as
+        I could. So I served a 2 year mission for The Church of Jesus Christ of Latter-day
+        Saints. I was assigned to serve in the California Ventura Mission. Those two years
+        turned my belief into a conversion. As I taught the principles of the gospel as
+        they are revealed to us through a modern prophet and as I dedicated every day for
+        two years to this and to living those principles I fell in love with it so much
+        I will never again leave it.</p>
     </body>
 </html>
