e03af14944e4124ed798f7badcaa57795ff43009
Brendon Welker <wel15018@byui.edu>
2019-01-19 22:36:35 -0700

    better half banner

--- web/aboutme/wife.php
+++ web/aboutme/wife.php
@@ -8,5 +8,39 @@
     <body>
         <?php include '../home/nav.php';?>
         <img class="banner" src="../pictures/Ashley-Wedding.jpg">
+        <h1>Ashley Nicol Welker</h1>
+        <p>I met my wife in Rexburg, Idaho. We lived in the same apartment
+        complex and attended the same congregation for our first 3 years
+        at Brigham Young University.</p>
+        <p>The first year I was friends with her
+           roomates and became her friend through them. I thought about asking
+           her on a date then but I was not sure if I was worthy to date someone
+           like her and did not know if she was even interested in me. Our second
+           year she actually asked me on our first date. We went on a double date
+           with one of her friends to get frozen yogurt. 
+        </p>
+        <p>I then asked her on a date and took her country dancing. We then 
+           dated for abour 2 months. I then regretably broke up with her.
+           After about 4 months I moved for a semester to Utah for an internship
+           in her home town. After a couple of weeks of being around eachother again
+           I decided I wanted to date her again. I still sometimes cannot believe she
+           took me back.
+        </p>
+        <div class="side-to-side">
+            <div class="left">
+                <p>After about another year of dating I took her to California to visit my
+                    mission and introduce her to the wonderful people I met there. What she
+                    did not realize, I was planning to propose to her in California. Gratefully
+                    she said yes, and we were married 4 months later.
+                </p>
+            </div>
+            <div class="right">
+                <img src="../pictures/proposal.jpg">
+            </div>
+        </div>
+        <p>Ashley is the love of my life. She is everything I could ever ask for.
+            I count myself lucky every day to call her my wife. In a word she is
+            awsome!
+        </p>
     </body>
 </html>
