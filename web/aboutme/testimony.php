9ad4a3b322758a9220921eb3c204922fd234ff4d
Brendon Welker <wel15018@byui.edu>
2019-01-19 18:18:34 -0700

    adjusting testimony page

--- web/aboutme/testimony.php
+++ web/aboutme/testimony.php
@@ -3,6 +3,7 @@
     <head>
         <title>Brendon Welker: Testimony</title>
         <link rel="stylesheet" href="../home/navbar.css">
+        <link rel="stylesheet" href="testimony.css">
     </head>
     <body>
         <?php include '../home/nav.php';?>
