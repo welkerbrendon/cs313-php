0f81f199b8ac920f51e4f8bfb0037afec792583c
Brendon Welker <wel15018@byui.edu>
2019-01-19 19:13:35 -0700

    hobbies banner

--- web/aboutme/hobbies.php
+++ web/aboutme/hobbies.php
@@ -7,6 +7,5 @@
     <body>
         <?php include '../home/nav.php';?>
         <img class="banner" id="top" src="../pictures/vikings.jpg">
-        <img class="banner" id="bottom" src="../pictures/mustang.jpg">
     </body>
 </html>
