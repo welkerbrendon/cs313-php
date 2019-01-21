7eb17dfcaa744ef4da0951c9a487589c5e0453e9
Brendon Welker <wel15018@byui.edu>
2019-01-21 12:47:25 -0700

    hobbies page

--- web/aboutme/hobbies.php
+++ web/aboutme/hobbies.php
@@ -3,13 +3,50 @@
     <head>
         <title>Brendon Welker: Hobbies</title>
         <link rel="stylesheet" href="../home/navbar.css">
-        <link rel="stylesheet" href="hobbies.css"
+        <link rel="stylesheet" href="hobbies.css">
     </head>
     <body>
         <?php include '../home/nav.php';?>
         <div class="side-by-side">
-            <img class="banner" id="vikings" src="../pictures/vikings.jpg">
-            <img class="banner" id="stang" src="../pictures/mustang.jpg">
+            <img class="banner" id="left" src="../pictures/vikings.jpg">
+            <img class="banner" id="right" src="../pictures/mustang.jpg">
+        </div>
+        <h1>These are the hobbies of the great Brendon Welker!</h1>
+        <h2>FOOTBALL</h2>
+        <h3>Minnesota Vikings</h3>
+        <div class="side-by-side">
+            <p id="left">I have been a Minnesota Vikings fan all my life.
+                My dad has been a fan all his life. When he was
+                young and first started watching football the
+                Vikings quarterback was Fran Tarkenton. He is a
+                hall of fame quarterback known for his ability to
+                scramble for a very long time and still pass the ball.
+                I was then raised as a fan by my dad but I became a fan
+                myself mostly durring the Randy Moss era of the Vikings.</p>
+            <img src="../pictures/franTarkenton.jpg" id="right">
+        </div>
+        <div>
+            <img src="../pictures/boise-state.jpg" id="left">
+            <p id="right">I was born in Idaho so naturally I also became a
+                Boise State football fan. Most of my time watching college
+                football was during the era of Coach Chris Petterson who led
+                Boise State to the highest national rankings the shool has 
+                ever seen. He was also the coach when Boise State beat 
+                Oaklahoma in the Fiesta Bowl, the greates football game I have
+                ever seen.</p>
+        </div>
+        <div class="side-by-side">
+            <p id="left">I also love cars, specifically american muscle cars.
+                My favorite of all american muscle cars is the Ford Mustang.
+                I am currently working to restore the one depicted at the top
+                of the page. It is a 1966 Ford Mustang and I am restoring it for
+                my wife. My dream car (which I will hopefully build myself someday)
+                is a 1968 Ford Mustand with the 427 Single Over Head Camshaft with
+                a dual, 4 barrel, high rise intake; tri-y long tube headers; 6 speed,
+                manual transmission; Ford, 9 inch, positraction rear end; rack and
+                pinion steering; 4 wheel, power assist disc brakes; a 5 point
+                racing harness; and the Shelby white with blue racing stripes paint job.</p>
+            <img src="../pictures/68mustang.jpg" id="right">
         </div>
     </body>
 </html>
