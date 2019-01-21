3cdf27ca742b5d2e130a3c742e01043a90e7c4f8
Brendon Welker <wel15018@byui.edu>
2019-01-21 13:08:19 -0700

    hobbies page

--- web/aboutme/hobbies.php
+++ web/aboutme/hobbies.php
@@ -13,42 +13,63 @@
         </div>
         <h1>These are the hobbies of the great Brendon Welker!</h1>
         <h2>FOOTBALL</h2>
-        <h3>Minnesota Vikings</h3>
-        <div class="side-by-side">
-            <p id="left">I have been a Minnesota Vikings fan all my life.
-                My dad has been a fan all his life. When he was
-                young and first started watching football the
-                Vikings quarterback was Fran Tarkenton. He is a
-                hall of fame quarterback known for his ability to
-                scramble for a very long time and still pass the ball.
-                I was then raised as a fan by my dad but I became a fan
-                myself mostly durring the Randy Moss era of the Vikings.</p>
-            <img class="sized" src="../pictures/franTarkenton.jpg" id="right">
-        </div>
-        <h3>BOISE STATE</h3>
-        <div>
-            <img class="sized" src="../pictures/boise-state.jpg" id="left">
-            <p id="right">I was born in Idaho so naturally I also became a
-                Boise State football fan. Most of my time watching college
-                football was during the era of Coach Chris Petterson who led
-                Boise State to the highest national rankings the shool has 
-                ever seen. He was also the coach when Boise State beat 
-                Oaklahoma in the Fiesta Bowl, the greates football game I have
-                ever seen.</p>
-        </div>
-        <h3>FORD MUSTANG</h3>
-        <div class="side-by-side">
-            <p id="left">I also love cars, specifically american muscle cars.
-                My favorite of all american muscle cars is the Ford Mustang.
-                I am currently working to restore the one depicted at the top
-                of the page. It is a 1966 Ford Mustang and I am restoring it for
-                my wife. My dream car (which I will hopefully build myself someday)
-                is a 1968 Ford Mustand with the 427 Single Over Head Camshaft with
-                a dual, 4 barrel, high rise intake; tri-y long tube headers; 6 speed,
-                manual transmission; Ford, 9 inch, positraction rear end; rack and
-                pinion steering; 4 wheel, power assist disc brakes; a 5 point
-                racing harness; and the Shelby white with blue racing stripes paint job.</p>
-            <img class="sized" src="../pictures/68mustang.jpg" id="right">
-        </div>
+        <table border="1">
+            <tr>
+                <th><h3>Minnesota Vikings</h3></th>
+            </tr>
+            <tr>
+                <td>
+                    <p id="left">I have been a Minnesota Vikings fan all my life.
+                        My dad has been a fan all his life. When he was
+                        young and first started watching football the
+                        Vikings quarterback was Fran Tarkenton. He is a
+                        hall of fame quarterback known for his ability to
+                        scramble for a very long time and still pass the ball.
+                        I was then raised as a fan by my dad but I became a fan
+                        myself mostly durring the Randy Moss era of the Vikings.
+                    </p>
+                </td>
+                <td>
+                    <img class="sized" src="../pictures/franTarkenton.jpg" id="right">
+                </td>
+            </tr>
+            <tr>
+                <th><h3>BOISE STATE</h3></th>
+            </tr>
+            <tr>
+                <td>
+                    <img class="sized" src="../pictures/boise-state.jpg" id="left">
+                </td>
+                <td>
+                    <p id="right">I was born in Idaho so naturally I also became a
+                        Boise State football fan. Most of my time watching college
+                        football was during the era of Coach Chris Petterson who led
+                        Boise State to the highest national rankings the shool has 
+                        ever seen. He was also the coach when Boise State beat 
+                        Oaklahoma in the Fiesta Bowl, the greates football game I have
+                        ever seen.</p>
+                </td>
+            </tr>
+            <tr>
+                <th><h3>FORD MUSTANG</h3></th>
+            </tr>
+            <tr>
+                <td>
+                    <p id="left">I also love cars, specifically american muscle cars.
+                        My favorite of all american muscle cars is the Ford Mustang.
+                        I am currently working to restore the one depicted at the top
+                        of the page. It is a 1966 Ford Mustang and I am restoring it for
+                        my wife. My dream car (which I will hopefully build myself someday)
+                        is a 1968 Ford Mustand with the 427 Single Over Head Camshaft with
+                        a dual, 4 barrel, high rise intake; tri-y long tube headers; 6 speed,
+                        manual transmission; Ford, 9 inch, positraction rear end; rack and
+                        pinion steering; 4 wheel, power assist disc brakes; a 5 point
+                        racing harness; and the Shelby white with blue racing stripes paint job.</p>
+                </td>
+                <td>
+                    <img class="sized" src="../pictures/68mustang.jpg" id="right">
+                </td>
+            </tr>
+        </table>
     </body>
 </html>
