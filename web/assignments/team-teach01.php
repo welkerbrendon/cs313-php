94bc6db06f3dd5de66840078f39f1cd94311f020
Brendon Welker <wel15018@byui.edu>
2019-01-22 10:31:27 -0700

    response

--- web/assignments/team-teach01.php
+++ web/assignments/team-teach01.php
@@ -7,9 +7,9 @@
         <form action="team-teach01-response.php" method="post">
             Name: <input type="text"><br>
             Email: <input type="text"><br>
-            Major: <input type="radio" name="majors">CS<br>
-            <input type="radio" name="majors">WDD<br>
-            <input type="radio" name="majors">CIT<br>
+            Major: <input type="radio" name="majors">CS 
+            <input type="radio" name="majors">WDD 
+            <input type="radio" name="majors">CIT 
             <input type="radio" name="majors">CE<br>
             Comments: <textarea name="comments"></textarea><br>
             <input type="submit">
