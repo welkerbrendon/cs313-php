d4667373baeade4bcb898eb0e8515479c198bfae
Brendon Welker <wel15018@byui.edu>
2019-01-28 11:22:01 -0700

    working on shopping cart icon

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -3,6 +3,7 @@
     <head>
         <title>Brendon Welker: Assignment1</title>
         <link rel="stylesheet" href="../home/navbar.css">
+        <link rel="stylesheet" href="../assignment-css-docs/assignment1.css">
     </head>
     <body>
         <?php include '../home/nav.php';?>
