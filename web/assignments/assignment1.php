3d96f4c325123d4e36ef01d075447fdc92ad20bb
Brendon Welker <wel15018@byui.edu>
2019-01-28 11:05:40 -0700

    working on new nav bar

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="../home/navbar.css">
     </head>
     <body>
-        <?php include '../assignments/navAssignment1.php';?>
+        <?php include '../home/nav.php';?>
         <h1>COMING SOON!</h1>
     </body>
 </html>
