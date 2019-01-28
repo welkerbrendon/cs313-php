40b6637683d5456c2c8318f2c48005af402357f2
Brendon Welker <wel15018@byui.edu>
2019-01-28 13:55:00 -0700

    working on shopping cart icon

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -10,5 +10,49 @@
         <div id="shopping-cart">
             <img src="../pictures/shopping-cart.jpg" class="shopping-cart">
         </div>
+        <h1>Welcome to the greatest selection of classic Fords ever assebled!<h1>
+        <table>
+            <tr>
+                <th colspan="2">Mustangs</th>
+            </tr>
+            <tr>
+                <td><input type="checkbox" value="25000">$25,000.00</td>
+                <td><img src="../pictures/car-shopping/convertable.jpg"></td>
+                <td><p>1964 1/2 Ford Mustang Convertable</p>
+                    <p>300 CID Straight 6 Engine</p>
+                    <p>3 Speed C4 Automatic Transmission</p>
+                    <p>4 Manual Drum Brakes</p>
+                    <p>Power Steering</p>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" value="45000">$45,000.00</td>
+                <td><img src="../pictures/car-shopping/shelby350.jpg"></td>
+                <td><p>1964 Ford Mustang Shelby GT350</p>
+                    <p>271 HP 289 CID V8 Engine</p>
+                    <p>4 Speed Toploader Manual Transmission</p>
+                    <p>Front Disc Rear Drum Manual Brakes</p>
+                    <p>Manual Steering</p>
+                </td>
+            </tr><tr>
+                <td><input type="checkbox" value="65000">$65,000.00</td>
+                <td><img src="../pictures/car-shopping/shelby500.jpg"></td>
+                <td><p>1967 Ford Mustang Shelby GT500</p>
+                    <p>355 HP 427 CID V8 Engine</p>
+                    <p>4 Speed Toploader Manual Transmission</p>
+                    <p>Front Disc Rear Drum Power Brakes</p>
+                    <p>Power Steering</p>
+                </td>
+            </tr><tr>
+                <td><input type="checkbox" value="300000">$300,000.00</td>
+                <td><img src="../pictures/car-shopping/boss529.jpg"></td>
+                <td><p>1969 Ford Mustang Boss 429</p>
+                    <p>375 HP 429 CID V8 Engine</p>
+                    <p>4 Speed Toploader Manual Transmission</p>
+                    <p>4 Power Disc Brakes</p>
+                    <p>Power Steering</p>
+                </td>
+            </tr>
+        </table>
     </body>
 </html>
