b1e0ca6bb796ad93b56668106b77ab353fff3d12
Brendon Welker <wel15018@byui.edu>
2019-01-28 18:07:43 -0700

    working on cart preview

--- web/assignments/assignment1helpers/assignment1.js
+++ web/assignments/assignment1helpers/assignment1.js
@@ -1,11 +1,7 @@
  function addToCart(button) {
     var cart = document.getElementById("cart");
     var nodeElement = document.createElement("P");
-    var buttonElement = document.createElement("BUTTON");
-    buttonElement.setAttribute("onclick='removeFromCart(this)");
-    var inputButtonText = document.createTextNode("Remove");
-    buttonElement.appendChild(inputButtonText);
     var textElement= document.createTextNode(button.value);
-    nodeElement.appendChild(textElement).appendChild(buttonElement);
+    nodeElement.appendChild(textElement);
     cart.insertBefore(nodeElement, cart.childNodes[cart.childNodes.length - 3]);
 }
