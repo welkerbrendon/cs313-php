40d4188b703a48cf1576f450f5514a21a148692c
Brendon Welker <wel15018@byui.edu>
2019-01-28 23:13:32 -0700

    testing for problem

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -56,66 +56,71 @@
         </div>
         <h1>Welcome to the greatest selection of classic Fords ever assembled!</h1>
         <table>
-            <tr class="seperate">
-                <th colspan="2"><h2>Mustangs<h2></th>
-            </tr>
-            <tr>
-                <td><img src="../pictures/car-shopping/convertable.jpg" class="item"></td>
-                <td><p>1964 1/2 Ford Mustang Convertable</p>
-                    <p>300 CID Straight 6 Engine</p>
-                    <p>3 Speed C4 Automatic Transmission</p>
-                    <p>4 Manual Drum Brakes</p>
-                    <p>Power Steering</p>
-                </td>
-            </tr>
-            <tr class="seperate">
-                <td colspan="2">
-                    <form action="assignment1.php" method="post">
-                        $25,000.00
-                        <?php
-                            if(!in_array("1964.5 Convertable: $25,000", $_SESSION["items"])){
-                                echo("<input type='submit' value='Add To Cart' name='25000'>");
-                            }
-                        ?>
-                    </form>
-                </td>
-            </tr>
-            <tr>
-                <td><img src="../pictures/car-shopping/shelby350.jpg" class="item"></td>
-                <td><p>1965 Ford Mustang Shelby GT350</p>
-                    <p>271 HP 289 CID V8 Engine</p>
-                    <p>4 Speed Toploader Manual Transmission</p>
-                    <p>Front Disc Rear Drum Manual Brakes</p>
-                    <p>Manual Steering</p>
-                </td>
-            </tr>
-            <tr class="seperate">
-                <td colspan="2">$45,000.00<button class="add" onclick="addToCart(this)" value="45000">Add ToCart</button></td>
-            </tr>
-            <tr>
-                <td><img src="../pictures/car-shopping/shelby500.jpg" class="item"></td>
-                <td><p>1967 Ford Mustang Shelby GT500</p>
-                    <p>355 HP 427 CID V8 Engine</p>
-                    <p>4 Speed Toploader Manual Transmission</p>
-                    <p>Front Disc Rear Drum Power Brakes</p>
-                    <p>Power Steering</p>
-                </td>
-            </tr>
-            <tr class="seperate">
-                <td colspan="2">$65,000.00<button class="add" onclick="addToCart(this)" value="1967 GT500: $65,000">Add To Cart</button></td>
-            </tr>
-            <tr>
-                <td><img src="../pictures/car-shopping/boss429.png" ></td>
-                <td><p>1969 Ford Mustang Boss 429</p>
-                    <p>375 HP 429 CID V8 Engine</p>
-                    <p>4 Speed Toploader Manual Transmission</p>
-                    <p>4 Power Disc Brakes</p>
-                    <p>Power Steering</p>
-                </td>
-            </tr>
-            <tr class="seperate">
-                <td colspan="2">$300,000.00<button class="add" onclick="addToCart(this)" value="1969 Boss 429: $300,000">Add To Cart</button></td>
-            </tr>   
+            <form>
+                <tr class="seperate">
+                    <th colspan="2"><h2>Mustangs<h2></th>
+                </tr>
+                <tr>
+                    <td><img src="../pictures/car-shopping/convertable.jpg" class="item"></td>
+                    <td><p>1964 1/2 Ford Mustang Convertable</p>
+                        <p>300 CID Straight 6 Engine</p>
+                        <p>3 Speed C4 Automatic Transmission</p>
+                        <p>4 Manual Drum Brakes</p>
+                        <p>Power Steering</p>
+                    </td>
+                </tr>
+                <tr class="seperate">
+                    <td colspan="2">
+                        <form action="assignment1.php" method="post" id='item1'>
+                            $25,000.00
+                            <?php
+                                if(!in_array("1964.5 Convertable: $25,000", $_SESSION["items"])){
+                                    echo("<button type='checkbox' name='25000' value='1964.5 Convertable: $25,000'>Select for Cart");
+                                }
+                            ?>
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td><img src="../pictures/car-shopping/shelby350.jpg" class="item"></td>
+                    <td><p>1965 Ford Mustang Shelby GT350</p>
+                        <p>271 HP 289 CID V8 Engine</p>
+                        <p>4 Speed Toploader Manual Transmission</p>
+                        <p>Front Disc Rear Drum Manual Brakes</p>
+                        <p>Manual Steering</p>
+                    </td>
+                </tr>
+                <tr class="seperate">
+                    <td colspan="2">$45,000.00<button class="add" onclick="addToCart(this)" value="45000">Add ToCart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="../pictures/car-shopping/shelby500.jpg" class="item"></td>
+                    <td><p>1967 Ford Mustang Shelby GT500</p>
+                        <p>355 HP 427 CID V8 Engine</p>
+                        <p>4 Speed Toploader Manual Transmission</p>
+                        <p>Front Disc Rear Drum Power Brakes</p>
+                        <p>Power Steering</p>
+                    </td>
+                </tr>
+                <tr class="seperate">
+                    <td colspan="2">$65,000.00<button class="add" onclick="addToCart(this)" value="1967 GT500: $65,000">Add To Cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="../pictures/car-shopping/boss429.png" ></td>
+                    <td><p>1969 Ford Mustang Boss 429</p>
+                        <p>375 HP 429 CID V8 Engine</p>
+                        <p>4 Speed Toploader Manual Transmission</p>
+                        <p>4 Power Disc Brakes</p>
+                        <p>Power Steering</p>
+                    </td>
+                </tr>
+                <tr class="seperate">
+                    <td colspan="2">$300,000.00<button class="add" onclick="addToCart(this)" value="1969 Boss 429: $300,000">Add To Cart</button></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Add to Cart"></td>
+                </tr>
+            </form>
         </table>
     </body>
 </html>
--- web/assignments/assignment1helpers/assignment1.js
+++ web/assignments/assignment1helpers/assignment1.js
@@ -1,32 +1,5 @@
  function addToCart(button) {
-    var cart = document.getElementById("cart");
-    var nodeElement = document.createElement("P");
-    var textElement;
-    switch (button.value){
-        case "25000":
-            textElement = document.createTextNode("1964.5 Convertable: $25,000");
-            break;
-        case "45000":
-            textElement = document.createTextNode("1965 GT350: $45,000");
-            break;
-        case "65000":
-            textElement = document.createTextNode("1967 GT500: $65,000");
-            break;
-        case "300000":
-            textElement = document.createTextNode("1969 Boss 429: $300,000");
-            break;
-        default:
-            return;
-    }
-    nodeElement.appendChild(textElement);
-    cart.insertBefore(nodeElement, cart.firstChild);
-
-    var totalElement = document.getElementById("total");
-    var totalElementValue = parseInt(totalElement.getAttribute("value")) + parseInt(button.value);
-    totalElement.setAttribute("value", totalElementValue);
-    totalElement.innerHTML = "Total: $" + totalElementValue + ".00";
-
-    addToForms(textElement, totalElementValue);
+    
 }
 
 function addToForms(textElement, valueTotal) {
