f259e7c7f67b71aad85c586caadc91cee89ccc14
Brendon Welker <wel15018@byui.edu>
2019-01-29 15:48:24 -0700

    review page

--- web/assignments/review-cart.php
+++ web/assignments/review-cart.php
@@ -1,9 +1,19 @@
+<?php
+    $form = "";
+    foreach ($_SESSION["items"] as $item_added){
+        $form .= "$item_added<input type='checkbox' value=$item_added name='items_to_remove'>'Select for removal from cart";
+    }
+?>
 <!DOCTYPE html>
 <head>
     <title>Brendon Welker's Store: Review Purchases</title>
     <link rel="stylesheet" href="../home/navbar.css">
 </head>
 <body>
-    <?php include '../home/nav.php';?>
+    <?php include '../home/nav.php';
+    echo "<form action='checkout.php' method='post'>";
+    echo($form);
+    echo"<input type='submit' value='checkout'></form>?>";?>
+
 </body>
 </html>
