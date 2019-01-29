24e68365224ef5efc8cc9a6e70903a599a1807ac
Brendon Welker <wel15018@byui.edu>
2019-01-29 14:39:33 -0700

    preventing duplicates

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -10,19 +10,19 @@
             switch ($item){
                 case "1964.5 Convertable: $25,000":
                     array_push($_SESSION["items"], $item);
-                    array_push($_SESSION["total"] += 25000);
+                    array_push($_SESSION["total"], 25000);
                     break;
                 case "1965 GT350: $45,000":
                     array_push($_SESSION["items"], $item);
-                    array_push($_SESSION["total"] += 45000);
+                    array_push($_SESSION["total"], 45000);
                     break;
                 case "1967 GT500: $65,000":
                     array_push($_SESSION["items"], $item);
-                    array_push($_SESSION["total"] += 65000);
+                    array_push($_SESSION["total"], 65000);
                     break;
                 case "1969 Boss 429: $300,000":
                     array_push($_SESSION["items"], $item);
-                    array_push($_SESSION["total"] += 300000);
+                    array_push($_SESSION["total"], 300000);
                     break;
                 default:
                     break;
@@ -30,8 +30,8 @@
         }
     }
 
-    //$_SESSION["items"] = array_unique($_SESSION["items"]);
-    //$_SESSION["total"] = array_unique($_SESSION["total"]);
+    $_SESSION["items"] = array_unique($_SESSION["items"]);
+    $_SESSION["total"] = array_unique($_SESSION["total"]);
 ?>
 <!DOCTYPE html>
 <html>
