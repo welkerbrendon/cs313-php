f12521b3108a011f361be659ffdfd3007dc00242
Brendon Welker <wel15018@byui.edu>
2019-01-29 00:06:35 -0700

    working on php cart

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -138,7 +138,7 @@
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="2"><input type="submit" value="Add to Cart"></td>
+                    <td colspan="2"><input type="submit" value="Add to Cart" class="add-to-cart"></td>
                 </tr>
             </table>
         </form>
