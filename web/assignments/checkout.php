1b569579f84599087ab3be2e8f1122a06f91b14a
Brendon Welker <wel15018@byui.edu>
2019-01-29 22:13:05 -0700

    review page

--- web/assignments/checkout.php
+++ web/assignments/checkout.php
@@ -1,25 +1,21 @@
 <?php
     session_start();
     
-    $_SESSION["items"] = array();
+    $_SESSION["items"] = $_POST;
 
     $_SESSION["total"] = 0;
     foreach($_POST as $item){
         switch ($item){
             case "1964.5 Convertable: $25,000":
-                array_push($_SESSION["items"], $item);
                 $_SESSION["total"] += 25000;
                 break;
             case "1965 GT350: $45,000":
-                array_push($_SESSION["items"], $item);
                 $_SESSION["total"] += 45000;
                 break;
             case "1967 GT500: $65,000":
-                array_push($_SESSION["items"], $item);
                 $_SESSION["total"] += 65000;
                 break;
             case "1969 Boss 429: $300,000":
-                array_push($_SESSION["items"], $item);
                 $_SESSION["total"] += 300000;
                 break;
             default:
