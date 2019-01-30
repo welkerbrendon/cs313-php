26352de87fc86e5ee05137cf397a7bcafed60616
Brendon Welker <wel15018@byui.edu>
2019-01-29 18:03:11 -0700

    review page

--- web/assignments/assignment1.php
+++ web/assignments/assignment1.php
@@ -1,53 +1,5 @@
 <?php
     session_start();
-    if(empty($_SESSION)){
-        $_SESSION["items"] = array();
-    }
-    else if(!isset($_SESSION["items"])){
-        $_SESSION["items"] = array();
-    }
-
-    if(!empty($_POST["items"])){
-        foreach($_POST["items"] as $item){
-            switch ($item){
-                case "1964.5 Convertable: $25,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1965 GT350: $45,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1967 GT500: $65,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1969 Boss 429: $300,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    $_SESSION["total"] = 0;
-    $_SESSION["items"] = array_unique($_SESSION["items"]);
-    foreach($_SESSION["items"] as $itemForTotal){
-        switch ($itemForTotal){
-            case "1964.5 Convertable: $25,000":
-                $_SESSION["total"] += 25000;
-                break;
-            case "1965 GT350: $45,000":
-                $_SESSION["total"] += 45000;
-                break;
-            case "1967 GT500: $65,000":
-                $_SESSION["total"] += 65000;
-                break;
-            case "1969 Boss 429: $300,000":
-                $_SESSION["total"] += 300000;
-                break;
-            default:
-                break;
-        }
-    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -74,7 +26,7 @@
             </div>
         </div>
         <h1>Welcome to the greatest selection of classic Fords ever assembled!</h1>
-        <form action="../assignments/" method="post">
+        <form action="review-cart.php" method="post">
             <table>
                 <tr class="seperate">
                     <th colspan="2"><h2>Mustangs<h2></th>
--- web/assignments/review-cart.php
+++ web/assignments/review-cart.php
@@ -1,8 +1,53 @@
 <?php
-    $form;
-    // foreach ($_POST as $item_added){
-    //     $form .= "$item_added<input type='checkbox' value=$item_added name='items_to_remove[]'>'Select for removal from cart";
-    // }
+    session_start();
+    if(empty($_SESSION)){
+        $_SESSION["items"] = array();
+    }
+    else if(!isset($_SESSION["items"])){
+        $_SESSION["items"] = array();
+    }
+
+    if(!empty($_POST["items"])){
+        foreach($_POST["items"] as $item){
+            switch ($item){
+                case "1964.5 Convertable: $25,000":
+                    array_push($_SESSION["items"], $item);
+                    break;
+                case "1965 GT350: $45,000":
+                    array_push($_SESSION["items"], $item);
+                    break;
+                case "1967 GT500: $65,000":
+                    array_push($_SESSION["items"], $item);
+                    break;
+                case "1969 Boss 429: $300,000":
+                    array_push($_SESSION["items"], $item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    $_SESSION["total"] = 0;
+    $_SESSION["items"] = array_unique($_SESSION["items"]);
+    foreach($_SESSION["items"] as $itemForTotal){
+        switch ($itemForTotal){
+            case "1964.5 Convertable: $25,000":
+                $_SESSION["total"] += 25000;
+                break;
+            case "1965 GT350: $45,000":
+                $_SESSION["total"] += 45000;
+                break;
+            case "1967 GT500: $65,000":
+                $_SESSION["total"] += 65000;
+                break;
+            case "1969 Boss 429: $300,000":
+                $_SESSION["total"] += 300000;
+                break;
+            default:
+                break;
+        }
+    }
 ?>
 <!DOCTYPE html>
 <head>
