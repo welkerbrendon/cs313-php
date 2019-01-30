a9588d47d24d48358b2b2463e02a929c6f058ff9
Brendon Welker <wel15018@byui.edu>
2019-01-29 20:59:28 -0700

    review page

--- web/assignments/review-cart.php
+++ web/assignments/review-cart.php
@@ -10,25 +10,25 @@
     foreach($_POST["items"] as $item){
         switch ($item){
             case "1964.5 Convertable: $25,000":
-                if(in_array($item, $_SESSION["item"])){
+                if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
                 $_SESSION["total"] += 25000;
                 break;
             case "1965 GT350: $45,000":
-                if(in_array($item, $_SESSION["item"])){
+                if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
                 $_SESSION["total"] += 45000;
                 break;
             case "1967 GT500: $65,000":
-                if(in_array($item, $_SESSION["item"])){
+                if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
                 $_SESSION["total"] += 65000;
                 break;
             case "1969 Boss 429: $300,000":
-                if(in_array($item, $_SESSION["item"])){
+                if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
                 $_SESSION["total"] += 300000;
