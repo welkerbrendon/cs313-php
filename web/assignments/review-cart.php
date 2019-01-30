4bf95c665085c56fe32b3f338519d653cb85ed5c
Brendon Welker <wel15018@byui.edu>
2019-01-29 18:14:53 -0700

    review page

--- web/assignments/review-cart.php
+++ web/assignments/review-cart.php
@@ -1,32 +1,6 @@
 <?php
     session_start();
-    if(empty($_SESSION)){
-        $_SESSION["items"] = array();
-    }
-    else if(!isset($_SESSION["items"])){
-        $_SESSION["items"] = array();
-    }
-
-    if(!empty($_POST["items"])){
-        foreach($_POST["items"] as $item){
-            switch ($item){
-                case "1964.5 Convertable: $25,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1965 GT350: $45,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1967 GT500: $65,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                case "1969 Boss 429: $300,000":
-                    array_push($_SESSION["items"], $item);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+    $_SESSION["items"] = $_POST["items"];
 
     $_SESSION["total"] = 0;
     $_SESSION["items"] = array_unique($_SESSION["items"]);
@@ -48,6 +22,11 @@
                 break;
         }
     }
+
+    $form;
+    foreach($_SESSION["items"] as $item){
+        $form .= "$item<input type=checkbox name=items[] value=$item checked>Deselect to remove from purchase";
+    }
 ?>
 <!DOCTYPE html>
 <head>
@@ -56,9 +35,9 @@
 </head>
 <body>
     <?php include '../home/nav.php';
-    //echo("<form action='checkout.php' method='post'>");
-    echo("<p>" . $_SESSION["items"][0] . "</p>");
-    //echo("<input type='submit' value='checkout'></form>>");?>
+    echo("<form action='checkout.php' method='post'>");
+    echo($form);
+    echo("<input type='submit' value='checkout'></form>>");?>
 
 </body>
 </html>
