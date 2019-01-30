d0e731b0f07051688f483a3da034c7336d18d568
Brendon Welker <wel15018@byui.edu>
2019-01-29 21:44:16 -0700

    review page

--- web/assignments/checkout.php
+++ web/assignments/checkout.php
@@ -1,3 +1,33 @@
+<?php
+    session_start();
+    
+    $_SESSION["items"] = array();
+
+    $_SESSION["total"] = 0;
+    foreach($_POST as $item){
+        switch ($item){
+            case "1964.5 Convertable: $25,000":
+                array_push($_SESSION["items"], $item);
+                $_SESSION["total"] += 25000;
+                break;
+            case "1965 GT350: $45,000":
+                array_push($_SESSION["items"], $item);
+                $_SESSION["total"] += 45000;
+                break;
+            case "1967 GT500: $65,000":
+                array_push($_SESSION["items"], $item);
+                $_SESSION["total"] += 65000;
+                break;
+            case "1969 Boss 429: $300,000":
+                array_push($_SESSION["items"], $item);
+                $_SESSION["total"] += 300000;
+                break;
+            default:
+                break;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <head>
     <title>Brendon Welker's Store: Checkout</title>
@@ -5,5 +35,38 @@
 </head>
 <body>
     <?php include '../home/nav.php';?>
+    <h2>Please enter you mailing address for shipment of the following items:</h2>
+    <table>
+        <?php foreach($_SESSION["item"] as $item){
+            echo"<tr><td colspan='2'>$item</td></tr>";
+        } ?>
+    </table>
+    <form action="thankyou.html">
+        <table>
+            <tr>
+                <td>Full Name:</td>
+                <td><input type="text" size="50" maxlenth="50"></td>
+            </tr>
+            <tr>
+                <td>Stree Address:</td>
+                <td><input type="text" size="50" maxlenth="50"></td>
+            </tr>
+            <tr>
+                <td>City:</td>
+                <td><input type="text" size="50" maxlength="50"></td>
+            </tr>
+            <tr>
+                <td>State:</td>
+                <td><input type="text" size="2" maxlength="2"></td>
+            </tr>
+            <tr>
+                <td>Zip Code:</td>
+                <td><input type="text" size="5" maxlength="5"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" class="submit"></td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
---
+++ web/assignments/thankyou.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Thank you for shopping!</title>
+    </head>
+    <body>
+        <?php include '../home/nav.php';?>
+        <h1>Thank You!</h1>
+    </body>
+</html>
