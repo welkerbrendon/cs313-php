496c5fd1bed49935cba9845aa700c84f7ac70d3b
Brendon Welker <wel15018@byui.edu>
2019-01-29 21:06:23 -0700

    review page

--- web/assignments/review-cart.php
+++ web/assignments/review-cart.php
@@ -6,32 +6,27 @@
     }
 
     $_SESSION["total"] = 0;
-    //$_SESSION["items"] = array_unique($_SESSION["items"]);
     foreach($_POST as $item){
         switch ($item){
             case "1964.5 Convertable: $25,000":
                 if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
-                $_SESSION["total"] += 25000;
                 break;
             case "1965 GT350: $45,000":
                 if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
-                $_SESSION["total"] += 45000;
                 break;
             case "1967 GT500: $65,000":
                 if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
-                $_SESSION["total"] += 65000;
                 break;
             case "1969 Boss 429: $300,000":
                 if(!in_array($item, $_SESSION["item"])){
                     array_push($_SESSION["items"], $item);
                 }
-                $_SESSION["total"] += 300000;
                 break;
             default:
                 break;
@@ -40,6 +35,22 @@
 
     $form;
     foreach($_SESSION["items"] as $item){
+        switch ($item){
+            case "1964.5 Convertable: $25,000":
+                $_SESSION["total"] += 25000;
+                break;
+            case "1965 GT350: $45,000":
+                $_SESSION["total"] += 45000;
+                break;
+            case "1967 GT500: $65,000":
+                $_SESSION["total"] += 65000;
+                break;
+            case "1969 Boss 429: $300,000":
+                $_SESSION["total"] += 300000;
+                break;
+            default:
+                break;
+        }
         $form .= "$item<br><input type=checkbox name=items[] value=$item checked>Deselect to remove from purchase<br>";
     }
 ?>
