0ccdebeed753a55ad85cb7f6315f77039b1c45fc
Brendon Welker <wel15018@byui.edu>
2019-01-29 22:15:24 -0700

    review page

--- web/assignments/thankyou.php
+++ web/assignments/thankyou.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Thank you for shopping!</title>
+        <link rel="stylesheet" href="../home/navbar.css">
     </head>
     <body>
         <?php include '../home/nav.php';?>
