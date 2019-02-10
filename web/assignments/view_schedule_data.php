6ae9cb9a4dd0365bb0734970b943ef110034b14b
Brendon Welker <wel15018@byui.edu>
2019-02-09 23:04:30 -0700

    setting up pages

--- web/assignments/view_schedule_data.php
+++ web/assignments/view_schedule_data.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="../home/navbar.css">
     </title>
     <body>
+        <?php include '../home/nav.php';?>
         <h1>Please fill out form in order to see desired history</h1>
     </body>
 </html>
