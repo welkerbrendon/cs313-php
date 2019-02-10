a025e95287f5a33d90d79b2ba887c84ec43ca232
Brendon Welker <wel15018@byui.edu>
2019-02-09 23:12:00 -0700

    setting up pages

--- web/assignments/add_day.php
+++ web/assignments/add_day.php
@@ -6,8 +6,6 @@
     </title>
     <body>
         <?php include '../home/nav.php';?>
-        <h1>Welcome to Your Schedule Tracker!</h1>
-        <a href="view_schedule_data.php"><button>View History</button></a>
-        <a href="add_day.php"><button>Add New Day</button></a>
+        <h1>Comming Soon!</h1>
     </body>
 </html>
