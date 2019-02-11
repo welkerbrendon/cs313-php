d71c0500be2053bc54829fe473f3ae69c8768aa4
Brendon Welker <wel15018@byui.edu>
2019-02-11 14:57:12 -0700

    working on redirect

--- web/assignments/schedule_tracker.php
+++ web/assignments/schedule_tracker.php
@@ -2,14 +2,14 @@
     /*$signed_in = "<a href='view_schedule_data.php'><button>View History</button></a>
     <a href='add_day.php'><button>Add New Day</button></a>";
     $not_signed_in = "<a href='sign_in'><button>Sign In</button></a>";
-    $page_to_show = "";
+    $page_to_show = "";*/
     if(in_array("username", $_POST)){
         setcookie("username", $_POST["username"], time() + (60 * 30);
         $page_to_show = $signed_in;
     }
     else {
         $page_to_show = $not_signed_in;
-    }*/
+    }
 ?>
 <!DOCTYPE html>
 <html>
