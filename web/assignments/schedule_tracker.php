8f1b1a0d76e2506bcc9fd215365c97cb67025ea5
Brendon Welker <wel15018@byui.edu>
2019-02-11 14:55:08 -0700

    working on redirect

--- web/assignments/schedule_tracker.php
+++ web/assignments/schedule_tracker.php
@@ -1,5 +1,5 @@
 <?php
-    $signed_in = "<a href='view_schedule_data.php'><button>View History</button></a>
+    /*$signed_in = "<a href='view_schedule_data.php'><button>View History</button></a>
     <a href='add_day.php'><button>Add New Day</button></a>";
     $not_signed_in = "<a href='sign_in'><button>Sign In</button></a>";
     $page_to_show = "";
@@ -9,7 +9,7 @@
     }
     else {
         $page_to_show = $not_signed_in;
-    }
+    }*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,6 +20,6 @@
     <body>
         <?php include '../home/nav.php';?>
         <h1>Welcome to Your Schedule Tracker!</h1>
-        <?php echo $page_to_show; ?>
+        <?php //echo $page_to_show; ?>
     </body>
 </html>
