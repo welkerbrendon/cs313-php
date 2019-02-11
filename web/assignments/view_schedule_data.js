535bdebe2a2c4eaf0187a2ac492594c18c9132cc
Brendon Welker <wel15018@byui.edu>
2019-02-11 16:13:06 -0700

    working on redirect

--- web/assignments/view_schedule_data.js
+++ web/assignments/view_schedule_data.js
@@ -1,5 +1,6 @@
 function day(){
-    var input_item = document.createElement("input").setAttribute("type", "date").appendChild(document.createTextNode("Desired Day"));
+    var input_item = document.createElement("input").setAttribute("type", "date");
+    input_item.appendChild(document.createTextNode("Desired Day"));
 
     var form = document.getElementById("form");
     form.insertBefore(input_item, form.childNodes[form.childNodes.length - 2]);
