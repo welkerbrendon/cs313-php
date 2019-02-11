b7fb0aabaedf2bb53b4ab209c1b1b4e682629f42
Brendon Welker <wel15018@byui.edu>
2019-02-11 15:23:41 -0700

    working on redirect

--- web/assignments/schedule_tracker.php
+++ web/assignments/schedule_tracker.php
@@ -3,12 +3,12 @@
     <a href='add_day.php'><button>Add New Day</button></a>";
     $not_signed_in = "<a href='sign_in'><button>Sign In</button></a>";
     $page_to_show = "";
-    if(isset($_POST)){
+    if(isset($_POST["username"])){
         setcookie("username", $_POST["username"], time() + (60 * 30);
-        //$page_to_show = $signed_in;
+        $page_to_show = $signed_in;
     }
     else {
-        //$page_to_show = $not_signed_in;
+        $page_to_show = $not_signed_in;
     }
 ?>
 <!DOCTYPE html>
@@ -20,6 +20,6 @@
     <body>
         <?php include '../home/nav.php';?>
         <h1>Welcome to Your Schedule Tracker!</h1>
-        <?php //echo $page_to_show; ?>
+        <?php echo $page_to_show; ?>
     </body>
 </html>
--- web/assignments/sign_in.php
+++ web/assignments/sign_in.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <title>Schedule Tracker</title>
+        <link rel="stylesheet" href="../home/navbar.css">
     </head>
     <body>
+        <?php include '../home/nav.php';?>
         <h1>Please Sign In</h1>
         <form action="schedule_tracker.php" method="POST">
             username: <input type="text" maxlength="255" size="25"><br><br>
