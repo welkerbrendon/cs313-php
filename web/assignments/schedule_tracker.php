9b06c8f95a044b1e46c056fce25fb9f490cd63a2
Brendon Welker <wel15018@byui.edu>
2019-02-11 14:25:59 -0700

    setting up pages

--- web/assignments/schedule_tracker.php
+++ web/assignments/schedule_tracker.php
@@ -5,7 +5,7 @@
     $not_logged_in = "<h2>Please Sign In</h2>
     <form action='schedule_tracker.php' method='POST'>
     username: <input type='text' name='username' maxlength='255' size='25'><br><br>
-    password: <input type='text' name='password' maxlength='255' size='25'><br><br>
+    password: <input type='password' name='password' maxlength='255' size='25'><br><br>
     <input type='submit' value='Sign In'>
     </form>";
 
