338493727236a85c36b927681024fbf9d35ae380
Brendon Welker <wel15018@byui.edu>
2019-02-12 15:59:10 -0700

    getting mock data

--- web/assignments/php_project/creating_mock_data.php
+++ web/assignments/php_project/creating_mock_data.php
@@ -4,8 +4,8 @@
 
     $select_statement = $db->prepare("SELECT user_id FROM user_info WHERE username='welkerbrendon'");
     $select_statement->execute();
-    $uuid = $select_statement->fetch(PDO::FETCH_STRING);
+    $uuid = $select_statement->fetch(PDO::FETCH_ASSOC);
 
-    echo $uuid;
+    echo $uuid["user_id"];
     //$insert_statement = $db->prepare("INSERT INTO 
 ?>
