ca79e2878adf1589f0ee77db233b983a67b8ef6b
Brendon Welker <wel15018@byui.edu>
2019-02-12 15:57:14 -0700

    getting mock data

--- web/assignments/php_project/creating_mock_data.php
+++ web/assignments/php_project/creating_mock_data.php
@@ -1,18 +1,11 @@
 <?php
-    echo uniqid();
-    // require("connect_to_db.php");
-    // $db = connect();
+    require("connect_to_db.php");
+    $db = connect();
 
-    // $insert_statement = $db->prepare("INSERT INTO user_info (
-    //     user_id, username, account_password, last_active_time, creation_time)
-    //     VALUES (uuid(), welkerbrendon, welkerbrendon, now(), now()");
-    // $insert_statement->execute();
-    // $insert_uuid = $insert_statement->fetchAll(PDO::FETCH_ASSOC);
+    $select_statement = $db->prepare("SELECT user_id FROM user_info WHERE username='welkerbrendon'");
+    $select_statement->execute();
+    $uuid = $select_statement->fetch(PDO::FETCH_ASSOC);
 
-    // $insert_statement = $db->prepare("SELECT user_id FROM user_info");
-    // $insert_statement->execute();
-    // $uuid = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    // echo "Inserted UUID: $insert_uuid";
-    // echo "Recieved UUID: $uuid";
+    echo $uuid;
+    //$insert_statement = $db->prepare("INSERT INTO 
 ?>
