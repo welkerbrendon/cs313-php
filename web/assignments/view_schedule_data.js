88ab218aafac7ddf967c880c938cf7a52448fcca
Brendon Welker <wel15018@byui.edu>
2019-02-11 17:14:20 -0700

    working on getting input

--- web/assignments/view_schedule_data.js
+++ web/assignments/view_schedule_data.js
@@ -64,17 +64,16 @@ function custom(){
     input_item.setAttribute("id", "custom_input");
     input_item.appendChild(document.createTextNode("Start Date"));
 
-    var input_item1 = document.createElement("input");
-    input_item1.setAttribute("type", "date");
-    input_item1.setAttribute("id", "second_custom_input")
-    input_item1.appendChild(document.createTextNode("End Date"));
-
     var current_element = document.getElementById("custom_input");
     var form = document.getElementById("form");
     if(current_element){
         current_element.parentNode.replaceChild(input_item, current_element);
         var check_for_extra = document.getElementById("second_custom_input");
         if(!check_for_extra){
+            var input_item1 = document.createElement("input");
+            input_item1.setAttribute("type", "date");
+            input_item1.setAttribute("id", "second_custom_input")
+            input_item1.appendChild(document.createTextNode("End Date"));
             form.insertBefore(document.createElement("br"), form.childNodes[form.childNodes.length - 2]);
             form.insertBefore(document.createElement("br"), form.childNodes[form.childNodes.length - 2]);
         }
