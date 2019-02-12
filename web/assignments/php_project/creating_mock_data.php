e088b02a6c7704d826c58599b16660277a23f0c1
Brendon Welker <wel15018@byui.edu>
2019-02-12 16:50:34 -0700

    getting mock data

--- web/assignments/php_project/creating_mock_data.php
+++ web/assignments/php_project/creating_mock_data.php
@@ -7,7 +7,7 @@
     $uuid = $select_statement->fetch(PDO::FETCH_ASSOC);
 
     $given_day = NULL;
-    $string_insert_statement = "INSERT INTO day VALUES ($given_day, $uuid['user_id'], now(), now())";
+    $string_insert_statement = "INSERT INTO day VALUES";
     // for($i = 0; $i < 12; $i++){
     //     $given_day = date('d.m.y', strtotime("-$i days"));
     //     echo "$given_day<br>";
