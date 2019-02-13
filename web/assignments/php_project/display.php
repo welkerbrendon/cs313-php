c41931efc2673b2cb51de83e0f6629e40615ef69
Brendon Welker <wel15018@byui.edu>
2019-02-13 15:37:05 -0700

    getting queries working

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -43,7 +43,7 @@
     <body>
         <?php 
             include '../../home/nav.php';
-            echo print_r($data[0]["start_time"]);
+            echo print_r($data[0]);
         ?>
     </body>
 </html>
