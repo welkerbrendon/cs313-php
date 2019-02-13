d0534366b556ac57607110e23fd4f77b60cacd9d
Brendon Welker <wel15018@byui.edu>
2019-02-13 14:14:54 -0700

    getting queries working

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -33,7 +33,7 @@
         echo print_r(get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $start_of_month, $end_of_month));
     }
     else {
-        echo print_r(get_dats_in_window($_COOKIE["username"], $_COOKIE["password"], $_POST["start_date"], $_POST["end_date"]));
+        echo print_r(get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $_POST["start_date"], $_POST["end_date"]));
     }
 ?>
 <!DOCTYPE html>
