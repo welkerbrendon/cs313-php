ea38af920f898dc47a91d36a1ba7648cfbd8e29d
Brendon Welker <wel15018@byui.edu>
2019-02-12 22:01:59 -0700

    getting mock data

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -1,7 +1,21 @@
 <?php  
-    if(!isset($_POST[""])
+    if(!isset($_COOKIE["username"]){
+        header("Location: sign_in.php");
+    }
     require('connect_to_db.php');
     $db = connect();
+
+    $data = NULL;
+    if($_POST["time_period"]){
+        $given_day = $db->lastInsertId("day_given_day");
+        $query = $db->prepare("SELECT start_time end_time productive FROM activity 
+        INNER JOIN user_info ON user_info.user_name = $_COOKIE['username'], activity.user_id = user_info.user_id, activity.given_day = $given_day");
+
+        $query->execute();
+        $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        echo print_r($data);
+    }
 ?>
 <!DOCTYPE html>
 <html>
