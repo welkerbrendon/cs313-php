ef4171c2c80cc1efd5e75e6c9df6d6f7fb85dbfe
Brendon Welker <wel15018@byui.edu>
2019-02-12 18:55:18 -0700

    getting mock data

--- web/assignments/php_project/creating_mock_data.php
+++ web/assignments/php_project/creating_mock_data.php
@@ -21,7 +21,7 @@
             $end_time = ($hour - $i) . ":00";
             $start_time = ($hour - $i - 1) . ":00";
             $final_statement = "(uuid_generate_v4(), Cast('" . $uuid["user_id"] . "' as UUID), 
-            Cast('$given_day' as Date), Cast('$start_time' as Time), Cast('$end_time as Time), $productive)";
+            Cast('$given_day' as Date), Cast('$start_time' as Time), Cast('$end_time' as Time), $productive)";
 
             $insert_statement = $db->prepare($final_statement);
             $insert_statement->execute();
