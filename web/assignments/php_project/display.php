3afc9ad9fcc4e908261630a6df3d1c5d2dd521da
Brendon Welker <wel15018@byui.edu>
2019-02-12 22:07:23 -0700

    getting mock data

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -9,7 +9,7 @@
     if($_POST["time_period"] == "MostRecentDay"){
         $given_day = $db->lastInsertId("day_given_day");
         $query = $db->prepare("SELECT start_time end_time productive FROM activity 
-        INNER JOIN user_info ON user_info.user_name = $_COOKIE['username'], activity.user_id = user_info.user_id, activity.given_day = $given_day");
+        INNER JOIN user_info ON user_info.user_name = '" . $_COOKIE["username"] . "', activity.user_id = user_info.user_id, activity.given_day = $given_day");
 
         $query->execute();
         $data = $query->fetchAll(PDO::FETCH_ASSOC);
