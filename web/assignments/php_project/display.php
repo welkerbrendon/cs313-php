f96ebf20ccb61e925cd94cba2012550d0c3d44bc
Brendon Welker <wel15018@byui.edu>
2019-02-13 11:36:50 -0700

    getting display working

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -12,7 +12,7 @@
     $username = $_COOKIE["username"];
     $password = $_COOKIE["password"];
     try{
-    $uuid_query = $db->prepare("SELECT user_id FROM user_info WHERE username='$username', account_password='$password'");
+    $uuid_query = $db->prepare("SELECT user_id FROM user_info WHERE username=$username AND account_password=$password");
     $uuid_query->execute();
     $user_id = $uuid_query->fetch(PDO::FETCH_ASSOC);
 
