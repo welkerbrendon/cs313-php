2ddf61450a2de96da166c71abd5f323062642dfd
Brendon Welker <wel15018@byui.edu>
2019-02-13 20:41:18 -0700

    displaying data

--- web/assignments/php_project/creating_mock_data.php
+++ web/assignments/php_project/creating_mock_data.php
@@ -2,9 +2,9 @@
     require("connect_to_db.php");
     $db = connect();
 
-    // $select_statement = $db->prepare("SELECT user_id FROM user_info WHERE username='welkerbrendon'");
-    // $select_statement->execute();
-    // $uuid = $select_statement->fetch(PDO::FETCH_ASSOC);
+    $select_statement = $db->prepare("SELECT user_id FROM user_info WHERE username='welkerbrendon'");
+    $select_statement->execute();
+    $uuid = $select_statement->fetch(PDO::FETCH_ASSOC);
 
     // $select_statement = $db->prepare("SELECT given_day FROM day");
     // $select_statement->execute();
