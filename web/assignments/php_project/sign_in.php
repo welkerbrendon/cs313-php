072507764570a0fe08870b21766f8ccaf27b20b7
Brendon Welker <wel15018@byui.edu>
2019-02-14 14:35:30 -0700

    trying to simplify queries

--- web/assignments/php_project/sign_in.php
+++ web/assignments/php_project/sign_in.php
@@ -7,19 +7,19 @@
     </head>
     <body>
         <?php include '../../home/nav.php';?>
-        <h1>Please Sign In</h1>
+        <h1 id="not_nav">Please Sign In</h1>
         <?php
             if($_COOKIE["valid_user"] == "False"){
-                echo "<h3>*Invalid username and/or password.
+                echo "<h3 id='not_nav'>*Invalid username and/or password.
                 Feel free to use the link below to create a new account.*</h3>";
             }
         ?>
-        <form action="schedule_tracker.php" method="POST">
+        <form id="not_nav" class="error" action="schedule_tracker.php" method="POST">
             username: <input type="text" name="username" maxlength="255" size="25"><br><br>
             password: <input type="password" name="password" maxlength="255" size="25"><br><br>
             <input type="submit" value="Sign In">
         </form>
         <br>
-        <a href="create_account.php"><button>Create Account</button></a>
+        <a id="not_nav" href="create_account.php"><button>Create Account</button></a>
     </body>
 </html>
