6b70dbdc79458ff02a0cc0768682b8c4daaf41ef
Brendon Welker <wel15018@byui.edu>
2019-02-14 14:54:52 -0700

    trying to simplify queries

--- web/assignments/php_project/create_account.php
+++ web/assignments/php_project/create_account.php
@@ -7,13 +7,16 @@
     <head>
         <title>Create New Account</title>
         <link rel="stylesheet" href="/home/navbar.css">
+        <link rel="stylesheet" href="create_account.css">
     </head>
     <body>
         <?php include '../../home/nav.php';?>
-        <h1>Coming Soon!</h1>
-        <a href="sign_in.php"><h3>Please use me to get back to sign in page. 
-            Use username: welkerbrendon and password: 1234abcd for now.
-            Thank you!</h3>
-        </a>
+        <div class="not_nav">
+            <h1>Coming Soon!</h1>
+            <a href="sign_in.php"><h3>Please use me to get back to sign in page. 
+                Use username: welkerbrendon and password: 1234abcd for now.
+                Thank you!</h3>
+            </a>
+        </div>
     </body>
 </html>
