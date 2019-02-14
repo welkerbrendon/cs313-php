b5a4faaa86ffc4e71a820f7d9702a2aceecc7712
Brendon Welker <wel15018@byui.edu>
2019-02-14 10:05:49 -0700

    link to create account

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -6,24 +6,44 @@
         $data = get_most_recent_day($_COOKIE["username"], $_COOKIE["password"]);
     }
     else if($_POST["time_period"] == "day") {
-        $data = get_given_day($_COOKIE["username"], $_COOKIE["password"], $_POST["day"]);
+        if($_POST["dat"]){
+            $data = get_given_day($_COOKIE["username"], $_COOKIE["password"], $_POST["day"]);
+        }
+        else {
+            header("Location: view_schedule_data.php");
+        }
     }
     else if ($_POST["time_period"] == "week"){
-        $start_of_week = $_POST["start_of_week"];
-        $end_of_week = new DateTime($start_of_week);
-        $end_of_week->add(new DateInterval('P6D'));
-        $end_of_week = date_format($end_of_week, "Y-m-d");
+        if($_POST["start_of_week"]){}
+            $start_of_week = $_POST["start_of_week"];
+            $end_of_week = new DateTime($start_of_week);
+            $end_of_week->add(new DateInterval('P6D'));
+            $end_of_week = date_format($end_of_week, "Y-m-d");
 
-        $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $start_of_week, $end_of_week);
+            $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $start_of_week, $end_of_week);
+        }
+        else {
+            header("Location: view_schedule_data.php");
+        }
     }
     else if ($_POST["time_period"] == "month"){
-        $start_of_month = $_POST["month"] . "-01";
-        $end_of_month = $_POST["month"] . "-31";
+        if($_POST["month"]){
+            $start_of_month = $_POST["month"] . "-01";
+            $end_of_month = $_POST["month"] . "-31";
 
-        $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $start_of_month, $end_of_month);
+            $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $start_of_month, $end_of_month);
+        }
+        else {
+            header("Location: view_schedule_data.php");
+        }
     }
     else {
-        $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $_POST["start_date"], $_POST["end_date"]);
+        if($_POST["start_date"] && $_POST["end_date"]){
+            $data = get_days_in_window($_COOKIE["username"], $_COOKIE["password"], $_POST["start_date"], $_POST["end_date"]);
+        }
+        else {
+            header("Location: view_schedule_data.php");
+        }
     }
 ?>
 <!DOCTYPE html>
