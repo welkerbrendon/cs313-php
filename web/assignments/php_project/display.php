dd52f89fec61d63b2271e8bb3b0a7c106f83edd1
Brendon Welker <wel15018@byui.edu>
2019-02-14 09:31:17 -0700

    displaying data

--- web/assignments/php_project/display.php
+++ web/assignments/php_project/display.php
@@ -1,18 +1,4 @@
 <?php  
-    if(isset($_COOKIE["username"]) && isset($_COOKIE["password"])){
-        setcookie("username", $_COOKIE["username"], time() + (60 * 30));
-        setcookie("password", $_COOKIE["password"], time() + (60 * 30));
-    }
-    else {
-        header("Location: sign_in.php");
-        exit;
-    }
-    if($_COOKIE["valid_user"] == "True"){
-        setcookie("valid_user", "True", time() + (60 * 30));
-    }
-    else {
-        header("Location: sign_in.php");
-    }
     require('obtaining_data_functions.php');
 
     $data = NULL;
