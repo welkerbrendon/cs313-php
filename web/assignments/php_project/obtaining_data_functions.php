79feb64e16533874a82e27f4944cbc391d108a93
Brendon Welker <wel15018@byui.edu>
2019-02-13 21:39:47 -0700

    displaying data

--- web/assignments/php_project/obtaining_data_functions.php
+++ web/assignments/php_project/obtaining_data_functions.php
@@ -14,24 +14,24 @@
     function get_most_recent_day($username, $password){
         $db = connect();
         $user_id = get_user_id($username, $password, $db);
-        $most_recent_given_day = NULL;
+        $most_recent_day_id = NULL;
         $i = -1;
-        while($most_recent_given_day == NULL){
+        while($most_recent_day_id == NULL){
             $i++;
             $comparable_date = date('Y-m-d', strtotime("-$i days"));
-            $find_day = $db->prepare("SELECT given_day 
-                                      FROM activity 
+            $find_day = $db->prepare("SELECT id 
+                                      FROM day 
                                       WHERE given_day='$comparable_date'");
             $find_day->execute();
-            $most_recent_given_day = $find_day->fetch(PDO::FETCH_ASSOC);
-            $most_recent_given_day = $most_recent_given_day["given_day"];
+            $most_recent_day_id = $find_day->fetch(PDO::FETCH_ASSOC);
+            $most_recent_day_id = $most_recent_day_id["id"];
         }
         $query = $db->prepare("SELECT activity.start_time, activity.end_time, activity.productive, activity.activity_type, activity.notes, day.given_day 
                                FROM activity
                                INNER JOIN day
                                ON activity.user_id=Cast('$user_id' as UUID) 
-                               AND day.given_day=Cast('$most_recent_given_day' as Date)
-                               AND day.id=activity.day_id;
+                               AND day.id=Cast('$most_recent_day_id' as Date)
+                               AND day.id=activity.day_id
                                ORDER BY start_time ASC");
 
         $query->execute();
