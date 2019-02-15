956eac2b1e8b872c5b5b88c88ff135c853208b3d
Brendon Welker <wel15018@byui.edu>
2019-02-14 22:57:35 -0700

    working on adding days

--- web/assignments/php_project/add_day.php
+++ web/assignments/php_project/add_day.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Schedule Tracker</title>
-        <link rel="stylesheet" href="../home/navbar.css">
+        <link rel="stylesheet" href="/home/navbar.css">
     </head>
     <body>
         <?php include '../home/nav.php';?>
