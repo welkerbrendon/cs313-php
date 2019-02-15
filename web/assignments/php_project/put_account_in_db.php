b1bc958dea8a022c359e7f533fa140da080eb060
Brendon Welker <wel15018@byui.edu>
2019-02-14 22:39:53 -0700

    getting create account working

--- web/assignments/php_project/put_account_in_db.php
+++ web/assignments/php_project/put_account_in_db.php
@@ -27,7 +27,7 @@
                 exit;
             }
             else {
-                $post_statement = $db->prepare("INSERT INTO user_info (user_id, username, account_password, created_at, last_active_at)
+                $post_statement = $db->prepare("INSERT INTO user_info (user_id, username, account_password, created_at, last_active_time)
                                                 VALUES (:new_id, :username, :password, now(), now())");
                 $post_statement->bindValue(":username", $username, PDO::PARAM_STR);
                 $post_statement->bindValue(":new_id", "uuid_generate_v4()", PDO::PARAM_STR);
