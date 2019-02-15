702ba71ac35ce8870ea2b5c9274bad66b6503192
Brendon Welker <wel15018@byui.edu>
2019-02-15 12:01:49 -0700

    working on setting up ui of creating day

--- web/assignments/php_project/add_day.php
+++ web/assignments/php_project/add_day.php
@@ -18,13 +18,13 @@
         $end_time_in_minutes = $i + 30;
         $hour = intval($i / 60);
         $am_pm = ($hour < 12) ? "am" : "pm";
-        $hour = ($hour < 1) ? 12 : $hour;
+        $hour = ($hour < 1) ? 12 : ($hour > 12) ? $hour - 12 : $hour;
         $minutes = ($i % 60 == 0) ? "00" : "30";
         $start_time = "$hour:$minutes $am_pm";
         $hour = intval($end_time_in_minutes / 60);
         $am_pm = ($hour < 12) ? "am" : "pm";
-        $hour = ($hour < 1) ? 12 : $hour;
-        $minutes = $end_time_in_minutes % 60;
+        $hour = ($hour < 1) ? 12 : ($hour > 12) ? $hour - 12 : $hour;
+        $minutes = ($end_time_in_minutes % 60 == 0) ? "00" : "30";
         $end_time = "$hour:$minutes $am_pm";
         $table .= 
         "<tr>
