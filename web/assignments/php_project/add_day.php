e11cdc98849199eae5d1a270a579c3e88ad815a0
Brendon Welker <wel15018@byui.edu>
2019-02-14 23:29:37 -0700

    working on adding days

--- web/assignments/php_project/add_day.php
+++ web/assignments/php_project/add_day.php
@@ -5,7 +5,30 @@
         <link rel="stylesheet" href="/home/navbar.css">
     </head>
     <body>
-        <?php include '../../home/nav.php';?>
-        <h1>Comming Soon!</h1>
+        <div>
+            <?php include '../../home/nav.php';?>
+            <h1>Fill in the following information and click submit when done.</h1>
+            <form>
+                <label for="date">Date of Activities</label>
+                <input type="date" id="date" name="date">
+                <table border=1>
+                    <tr>
+                        <th>Start Time</th>
+                        <th>End Time</th>
+                        <th>Productive</th>
+                        <th>Activity Type</th>
+                        <th>Notes</th>
+                    </tr>
+                    <?php
+                        echo DateTime::createFromFormat("i", 30)
+                        // for($i = 0; $i < 48; $i++){
+                        //     echo 
+                        //     "<tr>
+                        //         <td>"
+                        // }
+                    ?>
+                </table>
+            </form>
+        </div>
     </body>
 </html>
