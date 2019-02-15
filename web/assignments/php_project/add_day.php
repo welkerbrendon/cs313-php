00882bf52a9b308671e7728f1ac721331639eb65
Brendon Welker <wel15018@byui.edu>
2019-02-14 23:12:44 -0700

    working on adding days

--- web/assignments/php_project/add_day.php
+++ web/assignments/php_project/add_day.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="/home/navbar.css">
     </head>
     <body>
-        <?php include '/home/nav.php';?>
+        <?php include '../../home/nav.php';?>
         <h1>Comming Soon!</h1>
     </body>
 </html>
