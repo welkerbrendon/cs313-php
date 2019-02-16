ac2ed45e60099283c24e69086730a2c426f390c8
Brendon Welker <wel15018@byui.edu>
2019-02-16 15:26:29 -0700

    working on setting up ui of creating day

--- web/assignments/php_project/add_day_to_db.php
+++ web/assignments/php_project/add_day_to_db.php
@@ -1,3 +1,17 @@
 <?php
-    echo print_r($_POST);
+    require_once("connect_to_db.php");
+    $db = connect();
+
+    $start_times = $_POST["start_time"];
+    $end_times = $_POST["end_time"];
+    $productive = $_POST["productive"];
+    $activity_type = $_POST["activity_type"];
+    $notes = $_POST["notes"];
+
+    $echo "$start_times<br>";
+    $echo "$end_times<br>";
+    $echo "$productive<br>";
+    $echo "$activity_type<br>";
+    $echo "$notes<br>";
+
 ?>
