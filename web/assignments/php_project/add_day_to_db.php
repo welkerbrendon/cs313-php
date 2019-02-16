9bd6d32111874935da416faf113b0003df86a8d0
Brendon Welker <wel15018@byui.edu>
2019-02-16 16:19:41 -0700

    working on setting up ui of creating day

--- web/assignments/php_project/add_day_to_db.php
+++ web/assignments/php_project/add_day_to_db.php
@@ -55,7 +55,7 @@
         $day_statement->bindValue(":user_id", $user_id, PDO::PARAM_STR);
 
         $day_statement->execute();
-        return $day_statement->lastInsertId("day_id");
+        return $db->lastInsertId("day_id_seq");
     }
 
     function get_type_id($type_name, $db){
