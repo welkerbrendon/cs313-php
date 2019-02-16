b97ffc03216425983ca0f77f4b02bacc3bec0805
Brendon Welker <wel15018@byui.edu>
2019-02-16 13:49:11 -0700

    working on setting up ui of creating day

--- web/assignments/php_project/add_day.js
+++ web/assignments/php_project/add_day.js
@@ -2,6 +2,6 @@ function add_row(){
     var table_row = document.getElementById("tr");
     var table = document.getElementById("table");
 
-    var new_row = table.insert_row(table.getElementsByTagName("tr").length - 1);
+    var new_row = table.insertRow(table.getElementsByTagName("tr").length - 1);
     new_row = table_row;
 }
