4c642adb8531e81894d9a50e5c322072b47ffb71
Brendon Welker <wel15018@byui.edu>
2019-02-16 14:26:24 -0700

    working on setting up ui of creating day

--- web/assignments/php_project/add_day.php
+++ web/assignments/php_project/add_day.php
@@ -76,7 +76,7 @@
                     ?>
                 </table>
                 <p><button onclick="return add_row()">Add Row</button><button onclick="return delete_row()">Delete Row</button></p>
-                <h3><input id="submit" type="submit" value="Save"></h3>
+                <h1><input id="submit" type="submit" value="Save"></h1>
             </form>
         </div>
     </body>
