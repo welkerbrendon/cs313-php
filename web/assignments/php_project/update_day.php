c480d6360763347558c5b0489f75e95990c88222
Brendon Welker <wel15018@byui.edu>
2019-02-25 23:08:01 -0700

    allowing editting of a day

--- web/assignments/php_project/update_day.php
+++ web/assignments/php_project/update_day.php
@@ -2,11 +2,12 @@
     require('connect_to_db.php');
     $db = connect();
 
-    $day = $_POST["date"];
+    echo print_r($_POST);
+
+    $day = $_POST["given_day"];
     $start_times = $_POST["start_time"];
     $end_times = $_POST["end_time"];
     $productive_array = $_POST["productive"];
     $activity_type = $_POST["activity_type"];
     $notes = $_POST["notes"];
-
 ?>
