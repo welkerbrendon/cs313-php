3a3330537de310dd86856106a7e334ced6714193
Brendon Welker <wel15018@byui.edu>
2019-02-25 20:08:08 -0700

    allowing editting of a day

--- web/assignments/php_project/schedule_tracker.php
+++ web/assignments/php_project/schedule_tracker.php
@@ -3,39 +3,32 @@
     $db = connect();
 
     if(isset($_POST["username"]) && isset($_POST["password"])){
-        echo "in if<br>";
         setcookie("username", htmlspecialchars($_POST["username"]), time() + (60 * 30));
         setcookie("password", htmlspecialchars($_POST["password"]), time() + (60 * 30));
         $user_id = get_user_id($_COOKIE["username"], $_COOKIE["password"], $db);
 
-        if($user_id["user_id"] != NULL){
-            echo "in if in if<br>";
+        if($user_id["id"]){
             setcookie("valid_user", "True", time() + (60 * 30));
         }
         else {
-            echo "in else in if<br>";
             setcookie("valid_user", "False", time() + (60 * 30));
-            //header("Location: sign_in.php");
+            header("Location: sign_in.php");
         }
     }
     else if(isset($_COOKIE["username"]) && isset($_COOKIE["password"])){
-        echo "in else if<br>";
         setcookie("username", $_COOKIE["username"], time() + (60 * 30));
         setcookie("password", $_COOKIE["password"], time() + (60 * 30));
         $user_id = get_user_id($_COOKIE["username"], $_COOKIE["password"], $db);
         if($user_id["user_id"]){
-            echo "in if in else if<br>";
             setcookie("valid_user", "True", time() + (60 * 30));
         }
         else {
-            echo "in else in else if<br>";
             setcookie("valid_user", "False", time() + (60 * 30));
-            //header("Location: sign_in.php");
+            header("Location: sign_in.php");
         }
     }
     else {
-        echo "in else<br>";
-        //header("Location: sign_in.php");
+        header("Location: sign_in.php");
         exit;
     }
 ?>
