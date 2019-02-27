76538caf9590193c4cc03ed5ff9283410168d690
Brendon Welker <wel15018@byui.edu>
2019-02-26 17:06:32 -0700

    allowing editting of a day

--- web/assignments/php_project/check_if_logged_in.php
+++ web/assignments/php_project/check_if_logged_in.php
@@ -1,6 +1,6 @@
 <?php
-    function check_if_logged_in(){
-        if(isset($_COOKIE["username"]) && isset($_COOKIE["password"]) && isset($_COOKIE["valid_uesr"]){
+    function check_if_logged_in() {
+        if(isset($_COOKIE["username"]) && isset($_COOKIE["password"]) && isset($_COOKIE["valid_uesr"])){
             setcookie("username", $_COOKIE["username"], time() + (60 * 30));
             setcookie("password", $_COOKIE["password"], time() + (60 * 30));
             setcookie("valid_user", "True", time() + (60 * 30));
