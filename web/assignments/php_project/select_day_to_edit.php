0978abcbf450206a71118236505844a64ea29b60
Brendon Welker <wel15018@byui.edu>
2019-02-28 09:55:32 -0700

    adding linkage to editing a day

--- web/assignments/php_project/select_day_to_edit.php
+++ web/assignments/php_project/select_day_to_edit.php
@@ -7,6 +7,7 @@
     <body>
         <?php include '../../home/nav.php';?>
         <div class="not_nav" method="GET">
+            <h1>Please let us know which day you wish to edit</h1>
             <form action="edit_day.php">
                 Day to edit: <input type="date" name="day">
                 <br><input type="submit">
