80030d5c586cf484481610aea2f2d6121dc2303c
Brendon Welker <wel15018@byui.edu>
2019-02-28 21:40:09 -0700

    fixing but with add day again

--- web/assignments/php_project/add_day_to_db.php
+++ web/assignments/php_project/add_day_to_db.php
@@ -90,8 +90,7 @@
         $day_already_exists->bindValue(":day_id", "$day-$user_id", PDO::PARAM_STR);
         $day_already_exists->execute();
 
-        echo $day_already_exists->fetch(PDO::FETCH_NUM);
-        if($day_already_exists->fetch(PDO::FETCH_NUM)){
+        if(!empty($day_already_exists->fetch(PDO::FETCH_NUM)){
             setcookie("bad_input", "already_exists", time() + 2);
             setcookie("day", $day, time() + 2);
             header("Location: add_day.php");
