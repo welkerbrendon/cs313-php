76ca5a6b5ea0a6157d61146e68d7320ccee61333
Brendon Welker <wel15018@byui.edu>
2019-02-28 22:56:10 -0700

    simplify some code

--- web/assignments/php_project/view_schedule_data.php
+++ web/assignments/php_project/view_schedule_data.php
@@ -1,19 +1,6 @@
 <?php
-    if(isset($_COOKIE["username"]) && isset($_COOKIE["password"])){
-        setcookie("username", $_COOKIE["username"], time() + (60 * 30));
-        setcookie("password", $_COOKIE["password"], time() + (60 * 30));
-    }
-    else {
-        header("Location: sign_in.php");
-        exit;
-    }
-    if($_COOKIE["valid_user"] == "True"){
-        setcookie("valid_user", "True", time() + (60 * 30));
-    }
-    else {
-        header("Location: sign_in.php");
-        exit;
-    }
+    require_once("check_if_logged_in.php");
+    check_if_logged_in();
 ?>
 <!DOCTYPE html>
 <html>
